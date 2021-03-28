fb8199705ce269864a4ba1755367f15b7a71c5b7
hazae41 <hazae41@gmail.com>
2021-03-29 01:03:36 +0200

    Java

---
+++ README.md
@@ -0,0 +1,21 @@
+# Randomer for Bukkit
+
+> Always find a good random place to (re)spawn a player
+
+#### [Download Randomer](https://github.com/saurusmc/randomer-bukkit/raw/master/build/libs/randomer-1.2.jar)
+
+### Features
+
+- Configurable
+- No commands, no permissions
+- Will find a place on first join
+- Will find a new place on each death, unless the player has a bed or an anchor
+- Will find a good uninhabited place with a good biome
+
+### Config
+
+```yaml
+min-chunk-distance: 1024
+max-chunk-distance: 2048
+max-inhabited-seconds: 3600
+```
---
+++ src/main/java/hazae41/randomer/Plugin.java
@@ -0,0 +1,104 @@
+package hazae41.randomer;
+
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Biome;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.spigotmc.event.player.PlayerSpawnLocationEvent;
+
+import java.util.Random;
+
+public class Plugin extends JavaPlugin implements Listener {
+  int minDistance;
+  int maxDistance;
+  int maxInhabited;
+
+  @Override
+  public void onEnable() {
+    super.onEnable();
+
+    saveDefaultConfig();
+
+    minDistance = getConfig().getInt("min-chunk-distance");
+    maxDistance = getConfig().getInt("max-chunk-distance");
+    maxInhabited = getConfig().getInt("max-inhabited-seconds");
+
+    getServer().getPluginManager().registerEvents(this, this);
+  }
+
+  static boolean isBad(Biome biome) {
+    switch (biome) {
+      case OCEAN:
+      case COLD_OCEAN:
+      case FROZEN_OCEAN:
+      case WARM_OCEAN:
+      case LUKEWARM_OCEAN:
+      case DEEP_COLD_OCEAN:
+      case DEEP_FROZEN_OCEAN:
+      case DEEP_WARM_OCEAN:
+      case DEEP_LUKEWARM_OCEAN:
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  static int getRandomInt(int min, int max) {
+    return min + new Random().nextInt(max - min + 1);
+  }
+
+  static boolean getRandomBoolean() {
+    return new Random().nextBoolean();
+  }
+
+  Location getRandomLocation(World world) {
+    int tries = 0;
+
+    while (true) {
+      tries++;
+
+      int cx = getRandomInt(minDistance, maxDistance);
+      int cxs = getRandomBoolean() ? 1 : -1;
+
+      int cz = getRandomInt(minDistance, maxDistance);
+      int czs = getRandomBoolean() ? 1 : -1;
+
+      Chunk chunk = world.getChunkAt(cxs * cx, czs * cz);
+      Block block = chunk.getBlock(7, 80, 7);
+
+      if (tries == 4096) {
+        getLogger().warning("Could not find a good place");
+        return block.getLocation();
+      }
+
+      if (chunk.getInhabitedTime() > maxInhabited * 20L)
+        continue;
+      if (isBad(block.getBiome()))
+        continue;
+
+      getLogger().info("Found a good place in " + tries + " tries.");
+      return block.getLocation();
+    }
+  }
+
+  @EventHandler
+  void onSpawn(PlayerSpawnLocationEvent e) {
+    if (e.getPlayer().hasPlayedBefore()) return;
+    World world = e.getSpawnLocation().getWorld();
+    if (world == null) throw new NullPointerException();
+    e.setSpawnLocation(getRandomLocation(world));
+  }
+
+  @EventHandler
+  void onRespawn(PlayerRespawnEvent e) {
+    if (e.isAnchorSpawn() || e.isBedSpawn()) return;
+    World world = e.getRespawnLocation().getWorld();
+    if (world == null) throw new NullPointerException();
+    e.setRespawnLocation(getRandomLocation(world));
+  }
+}
